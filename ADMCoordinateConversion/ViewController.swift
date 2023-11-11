2e6b832a05c8bc42b48834421a9e57c17324099a
Daniel-Higgott <daniel.higgott@me.com>
2023-11-11 20:25:58 +0000

    Initial Commit

---
+++ ADMCoordinateConversion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ADMCoordinateConversion
+//
+//  Created by Daniel Higgott on 11/11/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
